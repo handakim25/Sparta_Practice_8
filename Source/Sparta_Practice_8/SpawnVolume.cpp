85e1fc2b25597ea3db37f33c9800621427186b2e
handakim25 <handakim25@gmail.com>
2025-02-13 10:46:25 +0900

    Add: Random Spawn을 구현

---
+++ Source/Sparta_Practice_8/SpawnVolume.cpp
@@ -0,0 +1,81 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpawnVolume.h"
+
+#include "Components/BoxComponent.h"
+
+// Sets default values
+ASpawnVolume::ASpawnVolume()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	USceneComponent* SceneRoot = CreateDefaultSubobject<USceneComponent>("Root");
+	SetRootComponent(SceneRoot);
+
+	SpawningBox = CreateDefaultSubobject<UBoxComponent>("SpawningBox");
+	SpawningBox->SetupAttachment(SceneRoot);
+	
+	SpawnHeightOffset = 64.f;
+}
+
+// Called when the game starts or when spawned
+void ASpawnVolume::BeginPlay()
+{
+	Super::BeginPlay();
+
+	for (int i = 0; i < 10; i++)
+	{
+		SpawnActorInVolume(SpawnActor);
+	}
+}
+
+FVector ASpawnVolume::GetRandomPointInVolume() const
+{
+	FVector BoxExtent = SpawningBox->GetScaledBoxExtent();
+	FVector BoxOrigin = SpawningBox->GetComponentLocation();
+
+	int TryCount = RandomSpawnTryCount <= 0 ? 1 : RandomSpawnTryCount;
+	while (TryCount--)
+	{
+		FVector StartLocation = BoxOrigin + FVector(
+			FMath::FRandRange(-BoxExtent.X, BoxExtent.X),
+			FMath::FRandRange(-BoxExtent.Y, BoxExtent.Y),
+			BoxExtent.Z
+		);
+
+		FVector EndLocation = StartLocation + FVector::DownVector * BoxExtent.Z * 2;
+
+		FHitResult Hit;
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActor(this);
+
+		if (GetWorld()->LineTraceSingleByChannel(Hit, StartLocation, EndLocation, ECC_Visibility, QueryParams))
+		{
+			return Hit.ImpactPoint + FVector::UpVector * SpawnHeightOffset;
+		}
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Spawning Volume Failed"));
+	return BoxOrigin;
+}
+
+void ASpawnVolume::SpawnActorInVolume(TSubclassOf<AActor> SpawnActorClass)
+{
+	if (!SpawnActorClass) return;
+
+	GetWorld()->SpawnActor<AActor>(
+		SpawnActorClass,
+		GetRandomPointInVolume(),
+		FRotator::ZeroRotator
+	);
+}
+
+// Called every frame
+void ASpawnVolume::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
